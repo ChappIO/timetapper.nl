4905708df1350ed4e76c9d47406aad2a62dc4d57
Thomas Biesaart <thomas.biesaart@protonmail.com>
2023-02-27 17:44:51 +0100

    Remove debug code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React, {useEffect} from 'react';
 import {Activities} from "./components/Activities";
 import {Controls} from "./components/Controls";
 import {Timer} from "./components/Timer";
-import {useStore} from "./store";
 
 const noSleep = new NoSleep();
 
@@ -14,11 +13,6 @@ export const App = () => {
             await noSleep.enable();
         }, false);
     }, []);
-    // console.log(JSON.stringify(
-    //     useStore(state => state),
-    //     null,
-    //     2
-    // ));
     return (
         <>
             <Activities/>
